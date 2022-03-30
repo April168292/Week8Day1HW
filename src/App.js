90106bb1cb2d5e67f6ba79ba42fcf5584aead5c3
April168292 <aprilchantelwill@gmail.com>
2022-03-30 10:19:26 -0500

    reactday1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import Home from './views/Home';
+import About from './views/About';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: 'April',
+    }
+  }
+
+
+  render() {
+    return (
+    
+      <div> 
+         <Navbar myName={this.state.name} />
+        <h1>this is the main app</h1>
+
+        <Routes> 
+        <Route path= '/' element={<Home />}/>
+          <Route path= '/About' element={<About />}/>
+        </Routes>
+        <Card />
+        
+      </div>
+      
+        
+        
+        
+    )
+  }
 }
 
-export default App;
---
+++ src/components/Carousel.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Carousel extends Component {
+  render() {
+    return (
+      <div>
+          <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="..." className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+</div>
+      </div>
+    )
+  }
+}
